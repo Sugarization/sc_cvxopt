d541a7773a78ec070d9abe093d336ec70da439b2
zws_MacPro <zhangwsh@shanghaitech.edu.cn>
2025-12-28 16:01:27 +0800

    full pipeline (no batching, CUDA OOM)

--- logistic_splitting.py
+++ logistic_splitting.py
@@ -6,7 +6,7 @@ def grad_F(u, ds):
 
 def inner_gd(u_last, v_last, ds, params):
     # copy u N times to initialize the N concurrent subproblems
-    u = u_last.unsqueeze(0).repeat(ds.N, -1, -1) 
+    u = u_last.unsqueeze(0).repeat(ds.N, 1, 1) 
     for i in range(params.n_iter):
         # X_j is essentially x_j repeated K times, altogether packed as (N, K, d)
         Xj = ds.X.unsqueeze(1).expand(-1, ds.K, -1) 
@@ -33,12 +33,12 @@ def parallel_douglas_rachford(tau, inits, n_iter, inner_params, ds):
             u0(K, d): initial linear weights
     '''
     u_t = inits.u0
-    v_t = torch.zeros(ds.N, ds.K, ds.d)
+    v_t = inits.v0 
     for t in range(n_iter):
         # solve N argmins in parallel
         u_nt = inner_gd(u_t, v_t, ds, inner_params)
         # update v
-        v_t += tau * (grad_F(u_nt) - grad_F(u_t))
+        v_t += tau * (grad_F(u_nt, ds) - grad_F(u_t, ds))
         # update u
         u_t = 1 / (ds.alpha * ds.N) * (ds.x_hat + v_t.sum(dim = 0))
     return u_t 
